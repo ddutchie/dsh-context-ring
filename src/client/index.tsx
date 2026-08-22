f831f18854dcc3aace35d39524b72bb7238e808d
Gerard Slee <ddutchie@gmail.com>
2026-08-22 12:09:12 -0400

    feat: format million token context limits as 1M in ContextRingWidget

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -6,6 +6,7 @@ export * from "./ContextRing.js";
 
 function formatTokens(num?: number): string {
   if (!num) return "0";
+  if (num >= 1_000_000) return (num / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M";
   if (num >= 1000) return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
   return num.toLocaleString();
 }
